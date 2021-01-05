189a3a9ffb978cb21330dc2f07ef5d540eb85075
ElipticNight <secondary@aidan-byrne.org.uk>
2021-01-05 14:09:52 +0000

    added basic join game layout

--- src/views/JoinGame.vue
+++ src/views/JoinGame.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>
-    Join Game
+  <div class="window">
+    <div class="container">
+      <h1>Join Game</h1>
+      <input type="text" name="roomid" placeholder="Room ID">
+      <input type="button" value="Join">
+    </div>
   </div>
 </template>
 <script>
@@ -8,4 +12,21 @@ export default {
   name: "JoinGame"
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.window {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  .container {
+    height: 75vh;
+    width: 25vw;
+    border-radius: 50px;
+    border: solid 2px blue;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+  }
+}
+</style>
