4662ab2286d1e4f59b43d56e529931c89babeaca
ElipticNight <secondary@aidan-byrne.org.uk>
2021-01-05 14:13:02 +0000

    added instructions base layout

--- src/views/Instructions.vue
+++ src/views/Instructions.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    Instructions
+  <div class="window">
+    <div class="container">
+      <h1>Instructions</h1>
+      <h3>One</h3>
+      <h3>Two</h3>
+      <h3>Three</h3>
+      <h3>Four</h3>
+    </div>
   </div>
 </template>
 <script>
@@ -8,4 +14,21 @@ export default {
   name: "Instructions"
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.window {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  .container {
+    height: 75vh;
+    width: 50vw;
+    border-radius: 50px;
+    border: solid 2px blue;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+  }
+}
+</style>
