80b78597c38ff5d55b07b1fe6c07f3a838a8c3ec
ElipticNight <secondary@aidan-byrne.org.uk>
2021-01-05 14:06:25 +0000

    got room id from url

--- src/views/Play.vue
+++ src/views/Play.vue
@@ -1,19 +1,27 @@
 <template>
-  <div>
-    Play
+  <div class="window">
+    <h1>{{ $route.params.channel }}</h1>
   </div>
 </template>
 <script>
 export default {
   name: "Play",
   mounted() {
-    window.Echo.channel("channel").listen("hello", e => {
+    window.Echo.channel(this.$route.params.channel).listen("hello", e => {
       this.responses.push(e);
     });
-    window.Echo.channel("channel").listen("response", e => {
+    window.Echo.channel(this.$route.params.channel).listen("response", e => {
+      console.log(e);
       this.responses.push(e);
     });
   }
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.window {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+</style>
