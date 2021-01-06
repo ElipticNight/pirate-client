e077f293855126c5ccdd2479a3b92ce5cb4c2a10
ElipticNight <secondary@aidan-byrne.org.uk>
2021-01-06 16:13:48 +0000

    updated instructions with new base layout

--- src/views/Instructions.vue
+++ src/views/Instructions.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="base-window">
     <div class="base-container">
-      <h1>Instructions</h1>
-      <div class="instructions">
-      <h3>This is instructions on how to play the porate game</h3>
-      <h3>Each of these instructions will be a vue component</h3>
-      <h3>Lorem</h3>
-      <h3>Ipsum</h3>
+      <div class="base-title">
+        <h1>Instructions</h1>
+      </div>
+      <div class="base-content">
+        <div class="instructions">
+          <h3>This is instructions on how to play the pirate game</h3>
+          <h3>Each of these instructions will be a vue component</h3>
+          <h3>Lorem</h3>
+          <h3>Ipsum</h3>
+        </div>
       </div>
     </div>
   </div>
