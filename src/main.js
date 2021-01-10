f47567e3a0cc053e51eaaad269849f6e90620a56
ElipticNight <secondary@aidan-byrne.org.uk>
2021-01-10 15:07:18 +0000

    implimented vuex store

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import {store} from "./store/store";
 
 Vue.config.productionTip = false;
 
@@ -18,6 +19,7 @@ window.Echo = new Echo({
 });
 
 new Vue({
+  store: store,
   router,
   render: h => h(App)
 }).$mount("#app");
