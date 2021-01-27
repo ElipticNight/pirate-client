955270f332410ed7f44bc2ef4347b09d57da7493
ElipticNight <secondary@aidan-byrne.org.uk>
2021-01-27 11:00:41 +0000

    added stuling for ready screen

--- src/components/setup/Player.vue
+++ src/components/setup/Player.vue
@@ -1,5 +1,10 @@
 <template>
-    <div class="player" v-bind:class="{ ready: isReady }">{{ reference }}</div>
+    <div class="player-wrapper" v-bind:class="{ ready: isReady }">
+      <div class="player-main">
+        <div class="image"></div>
+        <div class="name">{{ reference }}</div>
+      </div>
+    </div>
 </template>
 
 <script>
@@ -14,11 +19,29 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.player {
+.player-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   height: 20vh;
   width: 15vh;
   background-color: brown;
   border-radius: 15px;
+  .player-main {
+    display: grid;
+    grid-template-rows: 17fr 3fr;
+    height: 18vh;
+    width: 13vh;
+    .image {
+      background-color: lightblue;
+    }
+    .name {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: black;
+    }
+  }
 }
 .ready {
   background-color: green;
--- src/components/setup/ReadyList.vue
+++ src/components/setup/ReadyList.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="base-window">
-    <div class="ready-container">
-      <div class="player-list">
-        <Player v-for="(status, name) in players" :key="name" :reference="name"></Player>
-      </div>
+  <div class="ready-container">
+    <div class="player-list">
+      <Player v-for="(status, name) in players" :key="name" :reference="name"></Player>
+    </div>
+    <div class="button-wrapper">
       <button @click="unready()" class="unready-button">Unready!</button>
+      <button @click="unready()" class="start-button" hidden>Start!</button>
     </div>
   </div>
 </template>
@@ -33,30 +34,48 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .ready-container{
-  .player-list{
+  display: grid;
+  grid-template-rows: 4fr 1fr;
   height: 75vh;
   width: 75vw;
   padding: 3vh;
   border-radius: 50px;
   border: solid 2px blue;
-  display: grid;
-  grid-template-rows: repeat(5, 1fr);
-  grid-template-columns: repeat(5, 1fr);
-  row-gap: 3vh;
-  justify-items: center;
-  align-items: center;
+  .player-list{
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+    row-gap: 3vh;
+    justify-items: center;
+    align-items: center;
   }
-  .unready-button {
-    height: 7vh;
-    width: 70vh;
-    background-color: rgb(2, 71, 2);
-    color: white;
-    border: none;
-    border-radius: 20px;
-    &:hover{
-      box-shadow: 0px 0px 1px 1px rgb(2, 71, 2);
+  .button-wrapper {
+    display: flex;
+    justify-content: center;
+    .unready-button {
+      height: 7vh;
+      width: 70vh;
+      background-color: rgb(2, 71, 2);
+      color: white;
+      border: none;
+      border-radius: 20px;
+      &:hover{
+        box-shadow: 0px 0px 1px 1px rgb(2, 71, 2);
+      }
+    }
+    .start-button {
+      height: 7vh;
+      width: 70vh;
+      margin-left: 5vh;
+      background-color: rgb(2, 71, 2);
+      color: white;
+      border: none;
+      border-radius: 20px;
+      &:hover{
+        box-shadow: 0px 0px 1px 1px rgb(2, 71, 2);
+      }
     }
   }
 }
