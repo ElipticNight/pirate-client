0c6d22c794a0192ce8f0039cc96143063d739451
ElipticNight <secondary@aidan-byrne.org.uk>
2021-01-28 09:44:30 +0000

    changed join game layout

--- src/views/JoinGame.vue
+++ src/views/JoinGame.vue
@@ -4,7 +4,14 @@
       <div class="base-title">
         <h1>Join Game</h1>
       </div>
-      <div class="base-content">
+      <div class="content">
+        <div class="avatar"></div>
+        <input
+          class="game-id-button"
+          type="text"
+          v-model="roomID"
+          placeholder="Avatar"
+        />
         <input
           class="game-id-button"
           type="text"
@@ -48,16 +55,32 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.game-id-button {
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid blue;
-  border-radius: 4px;
-  box-sizing: border-box;
-  padding: 12px 20px;
-  background-color: lightblue;
-  &:hover {
-    box-shadow: 0px 0px 1px 1px blue;
+.content {
+  display: flex;
+  flex: 6;
+  justify-content: space-around;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+  margin-bottom: 5%;
+  .avatar {
+    height: 75%;
+    width: 60%;
+    margin-bottom: 5%;
+    background-color: lightblue;
+  }
+  .game-id-button {
+    width: 100%;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid blue;
+    border-radius: 4px;
+    box-sizing: border-box;
+    padding: 12px 20px;
+    background-color: lightblue;
+    &:hover {
+      box-shadow: 0px 0px 1px 1px blue;
+    }
   }
 }
 </style>
