763e8e7b2e8da99f3148c05485523deeada2f429
ElipticNight <secondary@aidan-byrne.org.uk>
2021-01-29 14:36:38 +0000

    fixed player card layout

--- src/components/setup/Player.vue
+++ src/components/setup/Player.vue
@@ -1,8 +1,11 @@
 <template>
     <div class="player-wrapper" v-bind:class="{ ready: isReady }">
-      <div class="player-main">
-        <div class="image"></div>
-        <div class="name">{{ reference }}</div>
+      <div class="main-wrapper">
+        <div class="player-main">
+          <div class="spacer"></div>
+          <div class="image"></div>
+          <div class="name">{{ reference }}</div>
+        </div>
       </div>
     </div>
 </template>
@@ -27,19 +30,27 @@ export default {
   width: 70%;
   background-color: brown;
   border-radius: 15px;
-  .player-main {
-    display: grid;
-    grid-template-rows: 17fr 3fr;
-    height: 85%;
-    width: 80%;
-    .image {
-      background-color: lightblue;
-    }
-    .name {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      color: black;
+  padding: 0px;
+  .main-wrapper {
+    height: 110%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .player-main {
+      display: grid;
+      grid-template-rows: 1fr 17fr 2fr;
+      height: 85%;
+      width: 80%;
+      .image {
+        background-color: lightblue;
+      }
+      .name {
+        display: flex;
+        justify-content: center;
+        align-items: flex-end;
+        color: black;
+      }
     }
   }
 }
